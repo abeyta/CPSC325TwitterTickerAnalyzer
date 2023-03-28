581854c21478dfe9c8bb906561a7f903502924d6
Adrian Abeyta <aabeyta@zagmail.gonzaga.edu>
2023-03-28 09:57:31 -0700

    stubbed out app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Twitter Ticker Analyzer")
+st.write("This is a simple app to analyze the sentiment of tweets about a stock ticker.")
+st.write("$TSLA")
+# st.linechart(tsla_df)
+st.write("$AAPL")
+# st.linechart(aapl_df)
+st.write("$AMZN")
+# st.linechart(amzn_df)
+st.write("$NVDA")
+# st.linechart(nvda_df)
